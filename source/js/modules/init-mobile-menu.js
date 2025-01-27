425b049966adc53b0b92492ed8cff7deb192b6d1
Антон Епифанов <antonepifanov87@gmail.com>
2025-01-27 22:15:00 +0300

    Удаляет лишнее

--- source/js/modules/init-mobile-menu.js
+++
@@ -1,8 +0,0 @@
-export const initMobileMenu = () => {
-  const header = document.querySelector('.header');
-  const toggleButton = header.querySelector('[data-burger]');
-
-  toggleButton.addEventListener('click', () => {
-    header.classList.toggle('is-open');
-  });
-};
